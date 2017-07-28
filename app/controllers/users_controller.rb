08fbd1a76215f6896d66162befd780ea0365308d
Kelsey Malone <kelsey.malone@gmail.com>
2017-07-28 17:22:58 -0400

    Add method shells to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
+  def new
+  end
 
+  def create(user_params)
+  end
+
+  def show
+  end
+
+  def my_quests
+  end
+
+  private
+  def user_params
+  end
 end
