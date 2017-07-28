2036ceaf4eb2d75ccda3d5fcc3e2936e02ee404f
Kelsey Malone <kelsey.malone@gmail.com>
2017-07-28 17:50:04 -0400

    Update Results migration

--- db/migrate/20170728214217_create_results.rb
+++ db/migrate/20170728214217_create_results.rb
@@ -1,7 +1,10 @@
 class CreateResults < ActiveRecord::Migration[5.1]
   def change
     create_table :results do |t|
-
+      t.integer :round_id, null: false
+      t.integer :question_id, null: false
+      t.boolean :win, null: false
+      
       t.timestamps
     end
   end
