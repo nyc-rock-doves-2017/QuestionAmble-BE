10de9cf6a57726c2cb45fdbae135d8ba2681a2d5
Kelsey Malone <kelsey.malone@gmail.com>
2017-07-28 17:19:52 -0400

    Add method shells to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,21 @@
 class QuestionsController < ApplicationController
+  def new
+  end
 
+  def create(question_params)
+  end
+
+  def show
+  end
+
+  def update(question_params)
+  end
+
+  def delete
+  end
+
+  private
+  def question_params
+  end
+  
 end
