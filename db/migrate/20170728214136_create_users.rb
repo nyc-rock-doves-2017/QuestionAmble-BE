ea9d9443fc77140b0e0f16749fae06c0a1178e53
Kelsey Malone <kelsey.malone@gmail.com>
2017-07-28 17:54:24 -0400

    Update question migration

--- db/migrate/20170728214136_create_users.rb
+++ db/migrate/20170728214136_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username, null: false
       t.string :email, null: false
-      t.string :password, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
     end
--- db/migrate/20170728214223_create_questions.rb
+++ db/migrate/20170728214223_create_questions.rb
@@ -1,7 +1,13 @@
 class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions do |t|
-
+      t.integer :quest_id, null: false
+      t.text :question_text, null: false
+      t.text :answer, null: false
+      t.text :hint, null: false
+      t.string :clue_type, null: false
+      t.text :clue_text
+      
       t.timestamps
     end
   end
