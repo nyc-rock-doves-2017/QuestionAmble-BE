4dcd696ecc2ab4dd770ac037be73c86616827d1d
Kelsey Malone <kelsey.malone@gmail.com>
2017-08-01 14:22:01 -0400

    Create first_question method; simplify rounds post json return

--- app/serializers/round_serializer.rb
+++ app/serializers/round_serializer.rb
@@ -1,4 +1,4 @@
 class RoundSerializer < ActiveModel::Serializer
-  attributes :id, :created_by, :quest_title, :played_by, :completion_score, :accuracy_score, :date_of_play
+  attributes :id, :created_by, :quest_title, :played_by, :completion_score, :accuracy_score, :date_of_play, :first_question
   belongs_to :player, class_name: 'User'
 end
